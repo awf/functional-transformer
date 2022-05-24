e347d67dcabbadfb27ce07dce6ac20cbbcc30111
Andrew Fitzgibbon <awf@graphcore.ai>
2022-05-24 21:13:00 +0100

    black

--- main.py
+++ main.py
@@ -24,25 +24,26 @@ from jaxutils.dataset import TinyShakespeare
 from jaxutils.Adam import Adam
 from jaxutils.show_jaxpr import show_jaxpr_and_xla, show_xla, show_jaxpr
 
-jnp.set_printoptions(threshold=20,edgeitems=3,linewidth=2048,precision=3)
-np.set_printoptions(threshold=20, edgeitems=3,linewidth=2048,precision=3)
+jnp.set_printoptions(threshold=20, edgeitems=3, linewidth=2048, precision=3)
+np.set_printoptions(threshold=20, edgeitems=3, linewidth=2048, precision=3)
 
 # Noisily fail when arrays are the wrong size
 from jax.config import config
-config.update("jax_numpy_rank_promotion", "raise") 
 
-jit_sampler = Arg('jit-sampler', False)
+config.update("jax_numpy_rank_promotion", "raise")
+
+jit_sampler = Arg("jit-sampler", False)
 sample = transformer_sample_unjit
 if jit_sampler.peek():
-    sample = jax.jit(sample,static_argnums=(0,))
+    sample = jax.jit(sample, static_argnums=(0,))
 
 
-
-LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
-logger = logging.getLogger('pure-tranfomer')
+LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
+logger = logging.getLogger("pure-tranfomer")
 logger.setLevel(level=LOGLEVEL)
 timer = timer.get_timer(logging.WARNING)
 db = logger.debug
+
 
 def main():
 
@@ -51,23 +52,25 @@ def main():
     beta2 = Arg(flag="beta2", doc="Adam beta2", default=0.99)
     seq_len = Arg(flag="seq-len", doc="Sequence length", default=32)
     batch_size = Arg(flag="batch-size", doc="Batch size", default=128)
-    epochs = Arg('epochs', 32)
-    batches = Arg('batches', sys.maxsize, 'Max batches')
+    epochs = Arg("epochs", 32)
+    batches = Arg("batches", sys.maxsize, "Max batches")
 
     # Init the model params
-    heads = Arg('heads', 8, 'Number of attention heads')
-    d_model = Arg('dmodel', 512, 'Embedding dimension')
-    d_k = Arg('dk', 64, 'Attention head dimension')
-    d_ff = Arg('dff', 512, 'Feedforward layer dimension')
-    n_layers = Arg('layers', 3, 'Number of layers')
+    heads = Arg("heads", 8, "Number of attention heads")
+    d_model = Arg("dmodel", 512, "Embedding dimension")
+    d_k = Arg("dk", 64, "Attention head dimension")
+    d_ff = Arg("dff", 512, "Feedforward layer dimension")
+    n_layers = Arg("layers", 3, "Number of layers")
 
-    save = Arg('save', False, 'Save mode.  Log run to wandb, lengthen epochs and batches')
+    save = Arg(
+        "save", False, "Save mode.  Log run to wandb, lengthen epochs and batches"
+    )
 
     if save():
         wandb.init(project="pure-transformer", entity="awfidius", config=Arg.config())
     else:
-        print('Quick mode, disabling wandb')
-        wandb.init(mode='disabled')
+        print("Quick mode, disabling wandb")
+        wandb.init(mode="disabled")
         epochs.default = 2
         # Choose primes, to catch any size mismatches
         d_model.default = 93
@@ -81,51 +84,61 @@ def main():
     rnd_key = jax.random.PRNGKey(0)
     # Create dataset
     dataset = TinyShakespeare(rnd_key, seq_len=seq_len(), batch_size=batch_size())
-    tostr = lambda x: ''.join([dataset.itos[i] for i in x]).replace('\n', '\\n')
+    tostr = lambda x: "".join([dataset.itos[i] for i in x]).replace("\n", "\\n")
 
-    rnd_key,cfg,params = transformer_init(rnd_key, dataset.n_tokens, 
-                                d_model=d_model(), d_k=d_k(), n_layers=n_layers(), n_heads=heads(), d_ff=d_ff())
+    rnd_key, cfg, params = transformer_init(
+        rnd_key,
+        dataset.n_tokens,
+        d_model=d_model(),
+        d_k=d_k(),
+        n_layers=n_layers(),
+        n_heads=heads(),
+        d_ff=d_ff(),
+    )
 
-    names = [k for (k,_) in params.items()]
+    names = [k for (k, _) in params.items()]
     print(names)
     assert len(names) == len(jax.tree_flatten(params)[0])
 
-    #gnorms_table = wandb.Table(columns=names)
-    #wandb.log({'gnorms_table': gnorms_table})
+    # gnorms_table = wandb.Table(columns=names)
+    # wandb.log({'gnorms_table': gnorms_table})
 
-    sizes = jax.tree_map(lambda v:np.prod(v.shape), params)
-    sizes.print('sizes:')
-    print('Total parameter count:', np.sum(jax.tree_flatten(sizes)[0]))
+    sizes = jax.tree_map(lambda v: np.prod(v.shape), params)
+    sizes.print("sizes:")
+    print("Total parameter count:", np.sum(jax.tree_flatten(sizes)[0]))
     # sizes_table = wandb.Table(columns=['param','size'])
 
     @partial(jax.jit, static_argnums=0)
-    def loss_batch(cfg, params, seq): # seq: B x S
+    def loss_batch(cfg, params, seq):
         batched = vmap(transformer_loss, in_axes=(None, None, 0), out_axes=0)
         return jnp.mean(batched(cfg, params, seq))
-
 
     # show_jaxpr(get_loss_batch, (params, *islice(dataset,1)))
     grad_loss_batch_unjit = jax.grad(loss_batch, argnums=1)
     grad_loss_batch = jax.jit(grad_loss_batch_unjit, static_argnums=0)
 
-    matches = re.search('--xla_dump_to=([^ ]+)', os.environ.get('XLA_FLAGS') or '')
+    matches = re.search("--xla_dump_to=([^ ]+)", os.environ.get("XLA_FLAGS") or "")
     if matches:
         fn = matches[1] + "/grad_loss_batch.jaxpr.py"
         with open(fn, "w") as file:
             # xla = jax.xla_computation(loss_batch, static_argnums=0)(cfg, params, *islice(dataset,1))
             # print("XLA=", xla.as_hlo_text())
-            show_jaxpr(grad_loss_batch, (cfg, params, *islice(dataset,1)), file=file, static_argnums=0)
-        print('Saved jaxpr to', fn)
+            show_jaxpr(
+                grad_loss_batch,
+                (cfg, params, *islice(dataset, 1)),
+                file=file,
+                static_argnums=0,
+            )
+        print("Saved jaxpr to", fn)
 
     # grad_loss_batch = jax.pjit(grad_loss_batch_unjit, static_argnums=0)
 
-    optimizer = Adam(params, lr=lr(), betas=(beta1(),beta2()))
-
+    optimizer = Adam(params, lr=lr(), betas=(beta1(), beta2()))
 
     for _epoch in range(epochs()):
 
         # Iterate through batches
-        for i,data in enumerate(islice(dataset, sys.maxsize if save() else 10)):
+        for i, data in enumerate(islice(dataset, sys.maxsize if save() else 10)):
             # Compute and log the loss
             loss = loss_batch(cfg, params, data)
 
@@ -134,32 +147,40 @@ def main():
 
             # gnorms = jax.tree_map(lambda v:np.round(np.log((jnp.linalg.norm(v)))), grads)
             # if i == 0:
-            #     gnorms.print('gnorms:')       
+            #     gnorms.print('gnorms:')
 
             # gnorms,_ = jax.tree_flatten(gnorms)
             # gnorms_table.add_data(*gnorms)
             # plt.plot(gnorms)
             # plt.ylabel("gnorms")
 
-            print(wandb.run.name, 'loss',loss, 'sample', tostr(data[np.random.randint(0,len(data))])) # , 'gnorms', gnorms)
+            print(
+                wandb.run.name,
+                "loss",
+                loss,
+                "sample",
+                tostr(data[np.random.randint(0, len(data))]),
+            )  # , 'gnorms', gnorms)
             total_time = time.time() - start
 
-            wandb.log({"time": total_time, "batch": i, "loss": loss}) # 'gnorms': plt,  'gnorms_table': gnorms_table})
+            wandb.log(
+                {"time": total_time, "batch": i, "loss": loss}
+            )  # 'gnorms': plt,  'gnorms_table': gnorms_table})
 
             # Update parameters
             params = optimizer.step(params, grads)
 
         # Log a sample after each epoch
-        prompt = [dataset.stoi[c] for c in 'Au']
-        with timer('sample'):
-            sampled = sample(cfg, params, jnp.array(prompt))[len(prompt):]
-            print(loss, tostr(prompt) + '|' + tostr(sampled))
+        prompt = [dataset.stoi[c] for c in "Au"]
+        with timer("sample"):
+            sampled = sample(cfg, params, jnp.array(prompt))[len(prompt) :]
+            print(loss, tostr(prompt) + "|" + tostr(sampled))
 
     # Grab Current Time After Running the Code
     end = time.time()
     total_time = end - start
-    print("TIME: "+ str(total_time))
+    print("TIME: " + str(total_time))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- transformer.py
+++ transformer.py
@@ -16,103 +16,129 @@ import jax.experimental.host_callback
 from jaxutils.Arg import Arg
 from jaxutils.ParamsDict import ParamsDict
 
+
 def rand(rng, f, shape, **kwargs):
     """
     Wrap jax.random.foo function to split the incoming rng, and return the new rng as well as the new RNG
 
     rng,vals = rand(rng, jax.random.uniform, (9,3), minval=-2.0, maxval=2.0)
     """
-    rng,rng1 = jax.random.split(rng)
-    return rng,f(rng1, shape, **kwargs)
+    rng, rng1 = jax.random.split(rng)
+    return rng, f(rng1, shape, **kwargs)
+
 
 def split(rng, *args):
     return jax.random.split(rng, *args)
 
+
 def crossentropy(output: jnp.ndarray, target: int):
     return -jax.nn.log_softmax(output)[target]
+
 
 def seq_crossentropy(output: jnp.ndarray, targets: jnp.ndarray):
     return vmap(crossentropy)(output, targets).mean()
 
+
 # Linear layer Wx + b
 def Linear_init0(rng: jax.random.KeyArray, in_features: int, out_features: int):
     params = ParamsDict()
-    rnd_range = 1 / in_features ** 0.5
-    params.weight = jax.random.uniform(rng, (in_features, out_features),  
-                                    minval=-rnd_range, maxval=rnd_range)
+    rnd_range = 1 / in_features**0.5
+    params.weight = jax.random.uniform(
+        rng, (in_features, out_features), minval=-rnd_range, maxval=rnd_range
+    )
 
     params.bias = jnp.zeros((out_features,))
     return params
+
 
 def linear_init_uniform(rng: jax.random.KeyArray, in_features: int, out_features: int):
     """
     Initialize a linear layer with uniform weights and zero bias
     """
     params = ParamsDict()
-    rnd_range = 1 / in_features ** 0.5
-    rng,params.weight = rand(rng, jax.random.uniform, (in_features, out_features),  
-                                    minval=-rnd_range, maxval=rnd_range)
+    rnd_range = 1 / in_features**0.5
+    rng, params.weight = rand(
+        rng,
+        jax.random.uniform,
+        (in_features, out_features),
+        minval=-rnd_range,
+        maxval=rnd_range,
+    )
 
     params.bias = jnp.zeros((out_features,))
-    return rng,params
+    return rng, params
+
 
 # Layer norm
 def layernorm_init_identity(shape):
     """
     Initialize an elementwise_linear layer with unit gain, zero bias
     """
-    return ParamsDict(
-        gain = jnp.ones(shape),
-        bias = jnp.zeros(shape)
-    )
+    return ParamsDict(gain=jnp.ones(shape), bias=jnp.zeros(shape))
+
 
 def linear_with_commented_shapes(params, x: jnp.ndarray):
-    return jnp.matmul(x, params.weight) + params.bias[None,:]
+    return jnp.matmul(x, params.weight) + params.bias[None, :]
     #                 L x N, N x M        |- 1 x M -|
     #      |----- L x M --------------|   |- ? x M ---------|
 
-def center_commented(x, eps:float = 1e-5):
+
+def center_commented(x, eps: float = 1e-5):
     """
     Return (x - mean(x))/std(x)
 
     """
-    assert len(x.shape) == 1 # Used only on vectors in this example
+    assert len(x.shape) == 1  # Used only on vectors in this example
     x_centered = x - x.mean()
-    var = (x_centered ** 2).mean()
+    var = (x_centered**2).mean()
     return x_centered / jnp.sqrt(var + eps)
+
 
 # Compact primitives for 'all on one slide'
 
+
 def linear(params, x: jnp.ndarray):
-    return x @ params.weight + params.bias[None,:]
+    return x @ params.weight + params.bias[None, :]
+
 
 def elementwise_linear(params, x: jnp.ndarray):
-    return params.gain[None,:] * x + params.bias[None,:]
-
-def center(x, eps = 1e-5):
-    return (x - x.mean())/(x.std() + eps)
+    return params.gain[None, :] * x + params.bias[None, :]
 
 
-flip_pe_coef = Arg('flip-pe', False, "Scale token embedding, not position embedding")
+def center(x, eps=1e-5):
+    return (x - x.mean()) / (x.std() + eps)
 
-def transformer_init(rng: jax.random.KeyArray, n_vocab: int, d_model: int, n_layers: int, n_heads: int, d_k : int, d_ff: int, max_len = 4096):
+
+flip_pe_coef = Arg("flip-pe", False, "Scale token embedding, not position embedding")
+
+
+def transformer_init(
+    rng: jax.random.KeyArray,
+    n_vocab: int,
+    d_model: int,
+    n_layers: int,
+    n_heads: int,
+    d_k: int,
+    d_ff: int,
+    max_len=4096,
+):
     # Build config struct for call
     config = ParamsDict()
     config.d_k = d_k
     config.heads = n_heads
     if flip_pe_coef():
-        config.lambda_e = d_model ** -0.5
+        config.lambda_e = d_model**-0.5
         config.lambda_pe = 1.0
     else:
-        config.lambda_e = d_model ** -0.5
+        config.lambda_e = d_model**-0.5
         config.lambda_pe = 1.0
-    config.tau = 1 / d_k ** 0.5
+    config.tau = 1 / d_k**0.5
 
     # Build initializers for params
     params = ParamsDict()
 
     # Create embedding layer
-    rng,params.embeddings = rand(rng, jax.random.normal, (n_vocab, d_model))
+    rng, params.embeddings = rand(rng, jax.random.normal, (n_vocab, d_model))
 
     # Positional encodings initialized to zeros
     params.positional_encodings = jnp.zeros((max_len, d_model))
@@ -126,25 +152,26 @@ def transformer_init(rng: jax.random.KeyArray, n_vocab: int, d_model: int, n_lay
         layer.heads = []
         for _ in range(n_heads):
             head = ParamsDict()
-            rng,head.query = linear_init_uniform(rng, d_model, d_k)
-            rng,head.key = linear_init_uniform(rng, d_model, d_k)
-            rng,head.value = linear_init_uniform(rng, d_model, d_model)
-            
+            rng, head.query = linear_init_uniform(rng, d_model, d_k)
+            rng, head.key = linear_init_uniform(rng, d_model, d_k)
+            rng, head.value = linear_init_uniform(rng, d_model, d_model)
+
             layer.heads.append(head)
 
         layer.norm_ff = layernorm_init_identity(d_model)
 
-        rng,layer.ffn1 = linear_init_uniform(rng, d_model, d_ff)
-        rng,layer.ffn2 = linear_init_uniform(rng, d_ff, d_model)
+        rng, layer.ffn1 = linear_init_uniform(rng, d_model, d_ff)
+        rng, layer.ffn2 = linear_init_uniform(rng, d_ff, d_model)
 
         params.layers.append(layer)
 
     # Final normalization and output layer
     params.pre_output_norm = layernorm_init_identity(d_model)
-    rng,params.output = linear_init_uniform(rng, d_model, n_vocab)
+    rng, params.output = linear_init_uniform(rng, d_model, n_vocab)
 
-    return rng,config,params
+    return rng, config, params
 
+# fmt: off
 def transformer(cfg, params, x: jnp.ndarray):
     """
     cfg: Config, from transformer_init, holds hyperparameters
@@ -161,7 +188,7 @@ def transformer(cfg, params, x: jnp.ndarray):
     # Start with token embeddings
     embeddings = cfg.lambda_e * params.embeddings[x, :]     # L x Dm
 
-    # Add positional encodings 
+    # Add positional encodings
     embeddings += cfg.lambda_pe * params.positional_encodings[:L, :]
 
     # Apply the transformer layers
@@ -174,7 +201,7 @@ def transformer(cfg, params, x: jnp.ndarray):
         # Multi-head self-attention
         for head in layer.heads:
 
-            # Project into this head's query/key space 
+            # Project into this head's query/key space
             query = linear(head.query, t1)                  # L x Dk
             key = linear(head.key, t1)                      # L x Dk
 
@@ -191,7 +218,7 @@ def transformer(cfg, params, x: jnp.ndarray):
         t2 = vmap(center)(embeddings)
         t2 = elementwise_linear(layer.norm_ff, t2)          # L x Dm
 
-        # Feedforward fully connected 
+        # Feedforward fully connected
         t2 = linear(layer.ffn1, t2)                         # L x Dff
         t2 = jax.nn.relu(t2)
         t2 = linear(layer.ffn2, t2)                         # L x Dm
@@ -204,7 +231,8 @@ def transformer(cfg, params, x: jnp.ndarray):
     embeddings = elementwise_linear(params.pre_output_norm, embeddings)
 
     # And linearly project to output dimension
-    return linear(params.output, embeddings)
+    return linear(params.output, embeddings)                # L x n_vocab 
+#fmt: on
 
 def transformer_loss(cfg, params, x):
     """
@@ -218,8 +246,9 @@ def transformer_loss(cfg, params, x):
 
     return seq_crossentropy(output[:-1], x[1:])
 
+
 def transformer_sample_unjit(cfg, params, seq: jnp.ndarray, length: int = 20):
-    
+
     for i in range(length):
         output = transformer(cfg, params, seq)
 
