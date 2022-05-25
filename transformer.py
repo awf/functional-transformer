2aeb4ff0cd2ccfdc319613c5bbf7112773e72be3
Andrew Fitzgibbon <awf@fitzgibbon.ie>
2022-05-25 10:04:46 +0100

    Doc

--- transformer.py
+++ transformer.py
@@ -19,13 +19,17 @@ from jaxutils.ParamsDict import ParamsDict
 
 def rand(rng, f, shape, **kwargs):
     """
-    Wrap jax.random.foo function to split the incoming rng, and return the new rng as well as the new RNG
+    Wrap jax.random.foo function to split the incoming rng, and return the new rng beside the payload
 
-    rng,vals = rand(rng, jax.random.uniform, (9,3), minval=-2.0, maxval=2.0)
+    rng = ... from previous code ...
+
+    rng, vals1 = rand(rng, jax.random.uniform, (9,3), minval=-2.0, maxval=2.0)
+    # ^-- rng is now newly split
+    rng, vals2 = rand(rng, jax.random.normal, (3,9))
+    # ^-- rng is split again
     """
     rng, rng1 = jax.random.split(rng)
     return rng, f(rng1, shape, **kwargs)
-
 
 def split(rng, *args):
     return jax.random.split(rng, *args)
